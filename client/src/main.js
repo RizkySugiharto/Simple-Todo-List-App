f9f32d9bb29ea375c43fbde09ef05657b0b13b21
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2024-06-29 19:38:59 +0700

    Add environment variable "BACKEND_URI" and add dependency "dotenv" to client-side

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 createApp(App).mount('#app')
--- client/vue.config.js
+++ client/vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     devServer: {
       proxy: {
         '/api': {
-          target: 'http://localhost:3000'
+          target: process.env.BACKEND_URI
         }
       }
     }
